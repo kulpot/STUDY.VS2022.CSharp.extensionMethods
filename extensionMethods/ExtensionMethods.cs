b4a02f5b6a20d34ad1ad64bb5344621ffdee2bd3
Kulpot <kulpotwahu@gmail.com>
2023-05-17 02:52:24 +0800

    Add project files.

---
+++ extensionMethods/ExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extensionMethods
+{
+    static class ExtensionMethods
+    {
+        public static int GetSpaceCount(this string instance)
+        {
+            int count = 0;
+
+            foreach(char c in instance)
+            {
+                if(c.Equals(' '))
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ extensionMethods/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace extensionMethods
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string myName = "Kulpot Sunny Z Wahu";//3 spaces
+            this.Text = myName.GetSpaceCount().ToString();
+        }
+    }
+}
